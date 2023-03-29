7b0f3a1f39d653d003475e99e69be674ad444880
Sameer Ahmed <sameerameer03172120395@gmail.com>
2023-03-29 16:15:12 -0700

    1st part complete

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-// function displayTime(){
-//     var dateTime = new Date();
-//     var hrs = dateTime.getHour();
-//     var min = dateTime.getMinutes();
-//     var sec = dateTime.getSeconds();
-//     var ampm = document.getElementById('ampm');
-    
-//     document.getElementById('hours').innerHTML = hrs;
-//     document.getElementById('minutes').innerHTML = min;
-//     document.getElementById('seconds').innerHTML = sec;
-// }
-// setInterval(displayTime, 10);
-
 function displayTime() {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let time = hours + ":" + minutes + ":" + seconds;
-    document.getElementById("clock").innerHTML = time;
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let am = 'AM';
+
+  if (hours > 12){
+      hours = hours -12;
   }
-  
-  setInterval(displayTime, 1000);
+  if (hours == 0){
+      hours = 12;
+  }
+  if (hours < 10){
+      hours = '0' + hours;
+  }
+  if (minutes < 10){
+      minutes = '0' + minutes;
+}
+if (seconds < 10){
+    seconds = '0' + seconds;
+}
+if (hours > 12){
+  am = PM
+}
+
+  let time = hours + ":" + minutes + ":" + seconds + " " + am;
+  document.getElementById("clock").innerHTML = time;
+}
+
+setInterval(displayTime, 1000);
